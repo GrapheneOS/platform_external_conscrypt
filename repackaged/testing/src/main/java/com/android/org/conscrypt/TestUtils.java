8b46aa9d27cef22be7dc28060e6473566c1014db
Adam Vartanian <flooey@google.com>
2019-02-20 12:47:59 +0000

    Revert "Fix app compat issue by not enabling TLS 1.3 by default."
    Re-enable TLS 1.3.  If necessary, we may need to add some targeted
    fixes for app compat in the future.
    
    This reverts commit 6d0e93ea9165bdfa6619092eef3ee7f67a68dccb.
    
    Bug: 124108905
    Test: cts -m CtsLibcoreTestCases
    Change-Id: I4c81ccbc0cee7eb0761617a9b3421b8a1aaa32f0

--- repackaged/testing/src/main/java/com/android/org/conscrypt/TestUtils.java
+++ repackaged/testing/src/main/java/com/android/org/conscrypt/TestUtils.java
@@ -52,6 +52,7 @@ import javax.net.ssl.SSLEngineResult;
 import javax.net.ssl.SSLException;
 import javax.net.ssl.SSLParameters;
 import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import com.android.org.conscrypt.java.security.StandardNames;
@@ -304,6 +305,16 @@ public final class TestUtils {
                     "setUseEngineSocket", SSLServerSocketFactory.class, boolean.class);
             method.invoke(null, conscryptFactory, useEngineSocket);
             return conscryptFactory;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void setUseSessionTickets(SSLSocket socket, boolean useTickets) {
+        try {
+            Class<?> clazz = conscryptClass("Conscrypt");
+            Method method = clazz.getMethod("setUseSessionTickets", SSLSocket.class, boolean.class);
+            method.invoke(null, socket, useTickets);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
