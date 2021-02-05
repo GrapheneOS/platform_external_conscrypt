1e28fe992aaad52b3cf8af93bf8f85a53f75816b
Pete Bentley <44170157+prbprbprb@users.noreply.github.com>
2021-02-05 13:52:45 +0000

    Unit tests for stats support classes.
    Cherry-picked from upstream commit #967.
    
    Plus some comment refactoring and null checking in OptionalMethod.
    
    Bug: 170317856
    Test: atest CtsLibcoreTestCases
    Change-Id: Ia6c048d1e8ae661b0ea170bdd2456a11a8f36847

--- repackaged/common/src/main/java/com/android/org/conscrypt/metrics/OptionalMethod.java
+++ repackaged/common/src/main/java/com/android/org/conscrypt/metrics/OptionalMethod.java
@@ -16,19 +16,31 @@
  */
 package com.android.org.conscrypt.metrics;
 
+import com.android.org.conscrypt.Internal;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import com.android.org.conscrypt.Internal;
 
 /**
- * Helper class to handle reflection methods loading and invoking.
- * Does not throw any exceptions and instead behaves as a no-op
- * in case method (or class) couldn't be loaded.
+ * Helper class to handle reflexive loading and invocation of methods which may be absent.
+ *
+ * @hide This class is not part of the Android public SDK API
+ * @hide This class is not part of the Android public SDK API
  */
 @Internal
-final class OptionalMethod {
+public final class OptionalMethod {
     private final Method cachedMethod;
 
+    /**
+     * Instantiates a new OptionalMethod
+     * Does not throw any exceptions if the class or method can't be loaded and instead
+     * behaves as a no-op.
+     *
+     * @param clazz the Class to search for methods on
+     * @param methodName the name of the {@code Method} on {@code clazz}
+     * @param methodParams list of {@code Classes} of the {@code Method's} parameters
+     *
+     * @throws NullPointerException if the method name or any paramater class is null
+     */
     public OptionalMethod(Class<?> clazz, String methodName, Class<?>... methodParams) {
         this.cachedMethod = initializeMethod(clazz, methodName, methodParams);
     }
@@ -36,8 +48,11 @@ final class OptionalMethod {
     private static Method initializeMethod(
             Class<?> clazz, String methodName, Class<?>... methodParams) {
         try {
+            for (Class<?> paramClass : methodParams) {
+                checkNotNull(paramClass);
+            }
             if (clazz != null) {
-                return clazz.getMethod(methodName, methodParams);
+                return clazz.getMethod(checkNotNull(methodName), methodParams);
             }
         } catch (NoSuchMethodException ignored) {
         }
@@ -55,5 +70,12 @@ final class OptionalMethod {
         } catch (InvocationTargetException ignored) {
         }
         return null;
+    }
+
+    private static <T> T checkNotNull(T reference) {
+        if (reference == null) {
+            throw new NullPointerException();
+        }
+        return reference;
     }
 }
