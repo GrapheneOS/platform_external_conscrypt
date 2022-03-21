59a91b8947bbf7f49c37edf12876e1f0ea8f5f7f
Kurt Alfred Kluever <kak@google.com>
2022-03-21 10:36:07 -0400

    Use assertThrows() in OptionalMethodTest.
    Cherry-picked from upstream PR #1058.
    
    Test: m
    Change-Id: Idd7b3c4ad4f6e30a1d81da534d6233b8fb4d14dc

--- repackaged/common/src/test/java/com/android/org/conscrypt/metrics/OptionalMethodTest.java
+++ repackaged/common/src/test/java/com/android/org/conscrypt/metrics/OptionalMethodTest.java
@@ -4,6 +4,7 @@ package com.android.org.conscrypt.metrics;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -33,9 +34,10 @@ public class OptionalMethodTest {
         assertNull(substring.invoke("input", 2, 5));
     }
 
-    @Test(expected = NullPointerException.class)
+    @Test
     public void nullMethodName() {
-        new OptionalMethod(String.class, null, int.class, int.class);
+        assertThrows(NullPointerException.class,
+                () -> new OptionalMethod(String.class, null, int.class, int.class));
     }
 
     @Test
