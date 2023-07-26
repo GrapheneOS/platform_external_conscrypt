6b4d0dd5e17936f5d17f67103993c319af630c9a
Pete Bentley <prb@google.com>
2023-07-26 14:48:38 +0000

    [DO NOT MERGE] Skip ProtocolTest in CTS.
    Test should be MTS only, does not enforce any CDD.
    
    Bug: 286993265
    Change-Id: I86708332a44588a75bb1c0a85c73f81714b2c699
    Test: TH

--- common/src/test/java/org/conscrypt/metrics/ProtocolTest.java
+++ common/src/test/java/org/conscrypt/metrics/ProtocolTest.java
@@ -2,6 +2,7 @@ package org.conscrypt.metrics;
 
 import static org.junit.Assert.assertSame;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -9,6 +10,7 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class ProtocolTest {
     @Test
+    @Ignore("Test intended for MTS only.")
     public void consistency() {
         for (Protocol protocol : Protocol.values()) {
             String name = protocol.name().replace('_', '.');
--- repackaged/common/src/test/java/com/android/org/conscrypt/metrics/ProtocolTest.java
+++ repackaged/common/src/test/java/com/android/org/conscrypt/metrics/ProtocolTest.java
@@ -3,6 +3,7 @@ package com.android.org.conscrypt.metrics;
 
 import static org.junit.Assert.assertSame;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -13,6 +14,7 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class ProtocolTest {
     @Test
+    @Ignore("Test intended for MTS only.")
     public void consistency() {
         for (Protocol protocol : Protocol.values()) {
             String name = protocol.name().replace('_', '.');
