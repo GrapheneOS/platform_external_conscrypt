5f9c71a58696350dce82e2b6d9d090fd0c928f37
Miguel Aranda <miguelaranda@google.com>
2025-04-30 03:37:12 -0700

    Use fast natives in Conscrypt.
    Adds @FastNative and @CriticalNative annotations to conscrypt's jni calls within NativeCrypto.java. Adds a macro for conditionally changing some native method signatures on android for CriticalNatives. Adds stubs for the annotations for the unrepackaged code.
    
    Test: presubmits and atest NativeCryptoArgTest
    Change-Id: Id74f960804dbdcd799e41409c7ab447b57352772

--- common/src/jni/main/cpp/conscrypt/native_crypto.cc
+++ common/src/jni/main/cpp/conscrypt/native_crypto.cc
@@ -54,6 +54,7 @@
 #include <optional>
 #include <type_traits>
 #include <vector>
+#include "jni.h"
 
 using conscrypt::AppData;
 using conscrypt::BioInputStream;
@@ -2622,8 +2623,7 @@ static void NativeCrypto_EVP_MD_CTX_cleanup(JNIEnv* env, jclass, jobject ctxRef)
     }
 }
 
-static void NativeCrypto_EVP_MD_CTX_destroy(JNIEnv* env, jclass, jlong ctxRef) {
-    CHECK_ERROR_QUEUE_ON_RETURN;
+static void NativeCrypto_EVP_MD_CTX_destroy(CRITICAL_JNI_PARAMS_COMMA jlong ctxRef) {
     EVP_MD_CTX* ctx = reinterpret_cast<EVP_MD_CTX*>(ctxRef);
     JNI_TRACE_MD("EVP_MD_CTX_destroy(%p)", ctx);
 
@@ -7753,8 +7753,7 @@ static SSL_SESSION* server_session_requested_callback(SSL* ssl, const uint8_t* i
     return ssl_session_ptr;
 }
 
-static jint NativeCrypto_EVP_has_aes_hardware(JNIEnv* env, jclass) {
-    CHECK_ERROR_QUEUE_ON_RETURN;
+static jint NativeCrypto_EVP_has_aes_hardware(CRITICAL_JNI_PARAMS) {
     int ret = 0;
     ret = EVP_has_aes_hardware();
     JNI_TRACE("EVP_has_aes_hardware => %d", ret);
@@ -9999,9 +9998,8 @@ static jlong NativeCrypto_SSL_get_timeout(JNIEnv* env, jclass, jlong ssl_address
     return result;
 }
 
-static jint NativeCrypto_SSL_get_signature_algorithm_key_type(JNIEnv* env, jclass,
+static jint NativeCrypto_SSL_get_signature_algorithm_key_type(CRITICAL_JNI_PARAMS_COMMA
                                                               jint signatureAlg) {
-    CHECK_ERROR_QUEUE_ON_RETURN;
     return SSL_get_signature_algorithm_key_type(signatureAlg);
 }
 
@@ -10474,7 +10472,7 @@ static jint NativeCrypto_SSL_get_error(JNIEnv* env, jclass, jlong ssl_address,
     return SSL_get_error(ssl, ret);
 }
 
-static void NativeCrypto_SSL_clear_error(JNIEnv*, jclass) {
+static void NativeCrypto_SSL_clear_error(CRITICAL_JNI_PARAMS) {
     ERR_clear_error();
 }
 
