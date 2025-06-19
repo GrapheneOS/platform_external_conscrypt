f783e9c36aa876a35e3eaa86e78343604bab5380
Thiébaud Weksteen <tweek@google.com>
2025-06-19 12:18:48 +1000

    Add uid parameter to CertificateTransparencyVerificationReported
    When reporting this metric, include the UID of the process. Only include
    the first UID (as opposed to any UID related to a Binder).
    
    Bug: 416416536
    Flags: EXEMPT bugfix
    Test: boot && adb shell cmd stats print-logs 1 &&
          atest NetSecConfigCertificateTransparencySctLogListTestCases
          Validate that the atom repoted include a valid UID
    Change-Id: I2ec62acfa39c720cd6aa12e9d1429c6eded7313e

--- repackaged/common/src/main/java/com/android/org/conscrypt/NativeCrypto.java
+++ repackaged/common/src/main/java/com/android/org/conscrypt/NativeCrypto.java
@@ -806,9 +806,7 @@ public final class NativeCrypto {
     @FastNative
     static native void X509_REVOKED_print(long bioRef, long x509RevokedCtx);
 
-    @android.compat.annotation.UnsupportedAppUsage
-    @FastNative
-    static native void X509_REVOKED_free(long x509RevokedCtx);
+    @FastNative static native void X509_REVOKED_free(long x509RevokedCtx);
 
     // --- X509_EXTENSION ------------------------------------------------------
 
